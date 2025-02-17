e987cba925ee222bb079738ac1d2b9f0d064596e
Shayne999 <shaynejuniorn@gmail.com>
2025-02-17 13:05:14 +0200

    Updated Middleware to parse request body

--- server/server.js
+++ server/server.js
@@ -17,8 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
 
 
 // Twitter API
